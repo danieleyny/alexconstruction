d5e6c7fba904c587c6aabe4e19fb15a36288b18f
Daniel eyny <danieleyny@daniels-mbp.home>
2021-08-31 07:09:57 -0400

    transition page load

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,9 @@ const navlinks = document.querySelector('.nav-links')
 menuBtn.addEventListener('click',()=>{
     navlinks.classList.toggle('mobile-menu')
 })
+
+
+
+window.addEventListener("load", () => {
+    document.querySelector("body").classList.add("loaded"); 
+   });
